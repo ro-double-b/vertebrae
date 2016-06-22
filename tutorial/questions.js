b7c02e40385252c9f3d65337c59cf7ac12b33db9
Robb Marsee <robb.marsee@gamil.com>
2016-06-22 14:30:52 -0700

    "last updates before pairing"

--- tutorial/questions.js
+++ tutorial/questions.js
@@ -4,3 +4,5 @@
 	// why does the myTweet.set('change:message', 'what?' not work)
 // 1-rendering
 	// what is the message attritibute on line 113
+// 2-views
+	// why when i initialize comment is the attributes setting each letter of the string to its own unique attribute?!
